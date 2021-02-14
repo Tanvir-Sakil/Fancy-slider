176bc9b35ff61117b90efc4efda674452d0907fd
Tanvir Sakil <tanbirshakil18@gmail.com>
2021-02-14 23:04:30 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,171 @@
+const imagesArea = document.querySelector('.images');
+const gallery = document.querySelector('.gallery');
+const galleryHeader = document.querySelector('.gallery-header');
+const searchBtn = document.getElementById('search-btn');
+const sliderBtn = document.getElementById('create-slider');
+const sliderContainer = document.getElementById('sliders');
+// selected image 
+let sliders = [];
+
+
+// If this key doesn't work
+// Find the name in the url and go to their website
+// to create your own api key
+const KEY = '15674931-a9d714b6e9d654524df198e00';
+
+// show images 
+
+
+document.getElementById('search')
+    .addEventListener("keypress", function(event) {
+    // event.preventDefault();
+    if (event.key == 'Enter')
+    {
+        document.getElementById("search-btn").click();
+    }
+});
+
+const showImages = (images) => {
+  imagesArea.style.display = 'block';
+  gallery.innerHTML = '';
+  // show gallery title
+  toggleSpiner()
+  galleryHeader.style.display = 'flex';
+  images.forEach(image => {
+    let div = document.createElement('div');
+    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    gallery.appendChild(div)
+    
+  })
+
+}
+
+const getImages = (query) => {
+      toggleSpiner()
+      const messageShow =document.getElementById('no-result');
+      messageShow.innerHTML = " ";
+     fetch(`https://pixabay.com/api/?key=${KEY}&q=${query}&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => 
+      {
+        
+        if(data.hits.length == 0)
+        {
+          showMessage()
+        }
+        showImages(data.hits)
+      })
+    .catch(err => console.log(err))
+   
+}
+
+let slideIndex = 0;
+const selectItem = (event, img) => {
+  let element = event.target;
+  element.classList.toggle('added');
+ 
+  let item = sliders.indexOf(img);
+  if (item === -1) {
+    sliders.push(img);
+  }
+   else {
+    // alert('Hey, Already added !')
+    // element.classList.remove('added');
+    sliders.splice(1,item);}
+  // }
+}
+var timer
+const createSlider = () => {
+  // check slider image length
+  console.log(sliders.length)
+  if (sliders.length < 2) {
+    alert('Select at least 2 image.')
+    return;
+  }
+  // crate slider previous next area
+  sliderContainer.innerHTML = '';
+  const prevNext = document.createElement('div');
+  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+  prevNext.innerHTML = ` 
+  <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+  <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+  `;
+
+  sliderContainer.appendChild(prevNext)
+  document.querySelector('.main').style.display = 'block';
+  // hide image aria
+  imagesArea.style.display = 'none';
+  let duration = document.getElementById('duration').value || 1000;
+  if(duration<0)
+  {
+    alert("Duration should not be negative")
+    duration = 1000;                                                                                                                                                                                                                                                                                                                             
+  }
+  sliders.forEach(slide => {
+    let item = document.createElement('div')
+    item.className = "slider-item";
+    item.innerHTML = `<img class="w-100"
+    src="${slide}"
+    alt="">`;
+    sliderContainer.appendChild(item)
+  })
+  changeSlide(0)
+  timer = setInterval(function () {
+    slideIndex++;
+    changeSlide(slideIndex);
+  }, duration);
+}
+
+// change slider index 
+const changeItem = index => {
+  changeSlide(slideIndex += index);
+}
+
+// change slide item
+const changeSlide = (index) => {
+
+  const items = document.querySelectorAll('.slider-item');
+  if (index < 0) {
+    slideIndex = items.length - 1
+    index = slideIndex;
+  };
+
+  if (index >= items.length) {
+    index = 0;
+    slideIndex = 0;
+  }
+
+  items.forEach(item => {
+    item.style.display = "none"
+  })
+
+  items[index].style.display = "block"
+}
+
+searchBtn.addEventListener('click', function () {
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search').value;
+  getImages(search)
+  sliders.length = 0;
+})
+
+sliderBtn.addEventListener('click', function () {
+  createSlider()
+})
+
+
+const toggleSpiner = () =>
+{
+    const spinner= document.getElementById("loading-spinner");
+    spinner.classList.toggle('d-none')
+}
+
+
+const showMessage = () =>
+{
+    const messageShow =document.getElementById('no-result');
+    messageShow.innerHTML =
+    `<h2 style="text-align:center;margin-top:40px;color:red">No such photos to show </h2> `
+}
